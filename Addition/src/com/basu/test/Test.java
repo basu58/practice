57ea5bb8e7f15b809ca1fa85eb7fab5e42e450d4
basud <basud@RudraPC>
2020-04-12 01:42:34 +0530

    hhhhh

---
+++ Addition/src/com/basu/test/Test.java
@@ -0,0 +1,13 @@
+package com.basu.test;
+
+import com.basu.beans.Util;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Util u=new Util();
+		System.out.println(u.getDate()+" Hello world..");
+
+	}
+
+}

2a099103c55173fbdddd7107f0accac264baf137
basud <basud@RudraPC>
2020-04-12 01:18:12 +0530

    modification of test

--- Addition/src/com/basu/test/Test.java
+++ Addition/src/com/basu/test/Test.java
@@ -1,9 +1,12 @@
 package com.basu.test;
 
+import com.basu.beans.Util;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Util u=new Util();
+		System.out.println(u.getDate());
 
 	}
 
